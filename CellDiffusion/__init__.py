43125f230e0496bee41ea579448ef6af69597b9d
Xiaochen Zhang <60606923+ShiltonZhang@users.noreply.github.com>
2025-10-31 16:00:49 +1100

    Add files via upload

--- CellDiffusion/__init__.py
+++ CellDiffusion/__init__.py
@@ -10,10 +10,7 @@ from .core import CellDiffusion
 from .models import Unet
 from .diffusion import DiffusionSampler
 from .utils import preprocess_adata, postprocess_results
-
-__version__ = "0.1.0"
-__author__ = "Xiaochen Zhang"
-__email__ = "xiaochen.zhang3@student.unimelb.edu.au"
+from ._version import __version__, __author__, __email__
 
 __all__ = [
     "CellDiffusion",
